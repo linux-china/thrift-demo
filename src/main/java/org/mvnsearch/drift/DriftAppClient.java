97dafe6c71f3bb078e849cd40dfedfd6e84f2b38
linux_china <libing.chen@gmail.com>
2019-03-04 20:12:03 -0800

    drift client app

---
+++ src/main/java/org/mvnsearch/drift/DriftAppClient.java
@@ -0,0 +1,33 @@
+package org.mvnsearch.drift;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.net.HostAndPort;
+import io.airlift.drift.client.DriftClientFactory;
+import io.airlift.drift.client.address.SimpleAddressSelector;
+import io.airlift.drift.client.address.SimpleAddressSelectorConfig;
+import io.airlift.drift.codec.ThriftCodecManager;
+import io.airlift.drift.transport.netty.client.DriftNettyClientConfig;
+import io.airlift.drift.transport.netty.client.DriftNettyMethodInvokerFactory;
+
+/**
+ * drift client app
+ *
+ * @author linux_china
+ */
+@SuppressWarnings("UnstableApiUsage")
+public class DriftAppClient {
+    public static void main(String[] args) {
+        DriftClientFactory driftClientFactory = driftClientFactory();
+        Scribe scribe = driftClientFactory.createDriftClient(Scribe.class).get();
+        Account account = scribe.save(new Account(1, "good"));
+        System.out.println(account.getName());
+    }
+
+
+    public static DriftClientFactory driftClientFactory() {
+        SimpleAddressSelectorConfig simpleAddressSelectorConfig = new SimpleAddressSelectorConfig();
+        simpleAddressSelectorConfig.setAddressesList(ImmutableList.of(HostAndPort.fromParts("localhost", 9090)));
+        DriftNettyMethodInvokerFactory<?> methodInvokerFactory = DriftNettyMethodInvokerFactory.createStaticDriftNettyMethodInvokerFactory(new DriftNettyClientConfig());
+        return new DriftClientFactory(new ThriftCodecManager(), methodInvokerFactory, new SimpleAddressSelector(simpleAddressSelectorConfig));
+    }
+}

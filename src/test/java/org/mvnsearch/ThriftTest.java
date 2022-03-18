470f3e96937a688e2eea9d9c2b7fbed66a3f87fa
linux_china <libing.chen@gmail.com>
2022-03-18 11:03:47 -0700

    Polishing

--- src/test/java/org/mvnsearch/ThriftTest.java
+++ src/test/java/org/mvnsearch/ThriftTest.java
@@ -4,6 +4,7 @@ package org.mvnsearch;
 import org.apache.thrift.TSerializer;
 import org.apache.thrift.protocol.TJSONProtocol;
 import org.junit.jupiter.api.Test;
+import uic.Contact;
 import uic.User;
 
 /**
@@ -13,11 +14,16 @@ public class ThriftTest {
 
     @Test
     public void testApp() throws Exception {
+        Contact contact = new Contact();
+        contact.email = "demo@example.com";
+        contact.phone = "1343434";
         User user = new User();
         user.setId(1);
         user.setFirstName("jacky");
         user.setLastName("chan");
         user.setAge(40);
+        user.setAvatar(new byte[]{1, 2});
+        user.setContact(contact);
         TSerializer serializer = new TSerializer(new TJSONProtocol.Factory());
         String json = serializer.toString(user);
         System.out.println(json);

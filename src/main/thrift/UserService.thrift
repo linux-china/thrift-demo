9e24f3cb45bb9f6aed0c65f3363062601ac8599d
linux_china <libing.chen@gmail.com>
2022-03-18 11:03:33 -0700

    Add more fields

--- src/main/thrift/UserService.thrift
+++ src/main/thrift/UserService.thrift
@@ -1,10 +1,19 @@
 namespace java uic
 
 struct User {
-    1: i32 id,
+    1: i32 id
     2: string firstName
     3: string lastName
     4: i32 age
+    5: Contact contact
+    6: binary avatar
+    7: bool vip
+    8: string alias
+}
+
+struct Contact {
+   1: string phone,
+   2: string email
 }
 
 service UserService {

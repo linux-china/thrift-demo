8a48ce3eacef91328b85b04866d19218fee5783e
linux_china <libing.chen@gmail.com>
2022-03-18 11:28:24 -0700

    Add more data

--- src/main/java/org/mvnsearch/UserServiceImpl.java
+++ src/main/java/org/mvnsearch/UserServiceImpl.java
@@ -2,6 +2,7 @@ package org.mvnsearch;
 
 import net.datafaker.Faker;
 import org.apache.thrift.TException;
+import uic.Contact;
 import uic.User;
 import uic.UserService;
 
@@ -14,10 +15,15 @@ public class UserServiceImpl implements UserService.Iface {
     @Override
     public User getUser(int id) throws TException {
         Faker faker = new Faker();
+        Contact contact = new Contact();
+        contact.setEmail(faker.internet().emailAddress());
+        contact.setPhone(faker.phoneNumber().cellPhone());
         User user = new User();
         user.setId(id);
         user.setFirstName(faker.name().firstName());
         user.setLastName(faker.name().lastName());
+        user.setAge(40);
+        user.setAvatar(new byte[]{1, 2, 3});
         return user;
     }
 }
